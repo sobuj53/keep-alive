df9502d4a8918fdad6e616244da9bb72da3893c6
sobuj53 <25761237+sobuj53@users.noreply.github.com>
2021-07-04 14:22:44 +0600

    Update keep-alive.sh

--- keep-alive.sh
+++ keep-alive.sh
@@ -3,9 +3,9 @@
 while true
 do
    echo "Preventing site from idling..."
+   host="https://$BASE_URL_OF_BOT"
    #set up $BASE_URL_OF_BOT var in heroku
-   wget -q -O/dev/null $BASE_URL_OF_BOT
+   wget -q -O/dev/null $host
    #can be
-   #host="https://$BASE_URL_OF_BOT"
    #curl --silent --output /var/log/keep-alive.txt -L $host 
 done
